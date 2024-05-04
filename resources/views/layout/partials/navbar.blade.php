8ddbf178fdb048c9f4a28ea190ba2285cc54bab0
IqbaalMuhamad <222112197@stis.ac.id>
2024-05-05 00:32:40 +0700

    update

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -5,7 +5,15 @@
                     <li><a href="{{ '/berita' }}">Berita</a></li>
                     <li><a href="{{ '/pelayanan' }}">Layanan</a></li>
                     <li><a href="{{ '/publikasi' }}">Publikasi</a></li>
-                    <li class="dropdown"><a href="#"><span>Informasi</span> <i
+                    <li class="dropdown"><a href="#"><span>Informasi</span> <i <li><a href="{{ '/' }}"
+                                    class="">Home</a></li>
+                    <li><a href="{{ route('profil.struktur-organisasi') }}">Profil</a></li>
+                    <li><a href="index.html#services">Services</a></li>
+                    <li><a href="index.html#portfolio">Portfolio</a></li>
+                    <li><a href="index.html#pricing">Pricing</a></li>
+                    <li><a href="index.html#team">Team</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li class="dropdown"><a href="#"><span>Dropdown</span> <i
                                 class="bi bi-chevron-down toggle-dropdown"></i></a>
                         <ul>
                             <li><a href="#">Statistik</a></li>
