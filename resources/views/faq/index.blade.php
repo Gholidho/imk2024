7e8c4f5e1e7bfd313e2c7f7b07f6e975c3cac1f6
meischazahra <meischazahra@gmail.com>
2024-05-26 18:17:49 +0700

    up faq dan pengaduan

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -1,23 +1,121 @@
 @extends('layout.main')
 
 @section('styles')
+<!-- Vendor CSS Files -->
+<link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet" />
+
+<!-- Main CSS File -->
+<link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
 @endsection
 
 @section('container')
-    <main class="main">
+<main class="main">
+    <!-- Page Title -->
+    <div class="page-title" data-aos="fade">
+        <div class="heading">
+            <div class="row d-flex justify-content-center text-center">
+                <!-- Service Box Section -->
+                <section id="faq" class="faq section">
+                    <!-- Section Title -->
+                    <div class="container section-title" data-aos="fade-up">
+                        <h2>FAQ</h2>
+                        <p>Frequently Asked Questions</p>
+                    </div><!-- End Section Title -->
+                    <div class="container">
+                        <div class="row gy-4">
 
-        <!-- Page Title -->
-        <div class="page-title" data-aos="fade">
-            <div class="heading">
-                <div class="container">
-                    <div class="row d-flex justify-content-center text-center">
-                        <div class="col-lg-8">
-                            <h1 class="">COMING SOOON</h1>
-                            <p class="mb-0">FAQ-FAQ bakal muncul disini</p>
+                            <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+                                <div class="service-details section" style="padding:0;">
+                                    <div class="service-box">
+                                        <h4>FAQ List</h4>
+                                        <div class="services-list">
+                                            <a href="#" class="active"><i class="bi bi-arrow-right-circle"></i><span>Web Design</span></a>
+                                            <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Product Management</span></a>
+                                            <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Graphic Design</span></a>
+                                            <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Marketing</span></a>
+                                        </div>
+                                    </div>
+                                    <!-- End Services List -->
+
+                                    <div class="service-box">
+                                        <h4>Download Catalog</h4>
+                                        <div class="download-catalog">
+                                            <a href="#"><i class="bi bi-filetype-pdf"></i><span>Catalog PDF</span></a>
+                                            <a href="#"><i class="bi bi-file-earmark-word"></i><span>Catalog DOC</span></a>
+                                        </div>
+                                    </div>
+                                    <!-- End Download Catalog -->
+
+                                    <div class="help-box d-flex flex-column justify-content-center align-items-center">
+                                        <i class="bi bi-headset help-icon"></i>
+                                        <h4>Have a Question?</h4>
+                                        <p class="d-flex align-items-center mt-2 mb-0">
+                                            <i class="bi bi-telephone me-2"></i>
+                                            <span>+1 5589 55488 55</span>
+                                        </p>
+                                        <p class="d-flex align-items-center mt-1 mb-0">
+                                            <i class="bi bi-envelope me-2"></i>
+                                            <a href="mailto:contact@example.com">contact@example.com</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-8" data-aos="fade-up" data-aos-delay="200" style="padding : 60px 0;">
+                                <div class="faq-container">
+                                    <div class="faq-item faq-active">
+                                        <h3><span class="num">1.</span> <span>Non consectetur a erat nam at lectus urna duis?</span></h3>
+                                        <div class="faq-content">
+                                            <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p>
+                                        </div>
+                                        <i class="faq-toggle bi bi-chevron-right"></i>
+                                    </div><!-- End Faq item-->
+
+                                    <div class="faq-item">
+                                        <h3><span class="num">2.</span> <span>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</span></h3>
+                                        <div class="faq-content">
+                                            <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                                        </div>
+                                        <i class="faq-toggle bi bi-chevron-right"></i>
+                                    </div><!-- End Faq item-->
+
+                                    <div class="faq-item">
+                                        <h3><span class="num">3.</span> <span>Dolor sit amet consectetur adipiscing elit pellentesque?</span></h3>
+                                        <div class="faq-content">
+                                            <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis</p>
+                                        </div>
+                                        <i class="faq-toggle bi bi-chevron-right"></i>
+                                    </div><!-- End Faq item-->
+
+                                    <div class="faq-item">
+                                        <h3><span class="num">4.</span> <span>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</span></h3>
+                                        <div class="faq-content">
+                                            <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                                        </div>
+                                        <i class="faq-toggle bi bi-chevron-right"></i>
+                                    </div><!-- End Faq item-->
+
+                                    <div class="faq-item">
+                                        <h3><span class="num">5.</span> <span>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</span></h3>
+                                        <div class="faq-content">
+                                            <p>Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in</p>
+                                        </div>
+                                        <i class="faq-toggle bi bi-chevron-right"></i>
+                                    </div><!-- End Faq item-->
+
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
+                <!-- End Service Box Section -->
             </div>
-        </div><!-- End Page Title -->
-    </main>
+        </div>
+    </div><!-- End Page Title -->
+</main>
 @endsection
--- resources/views/kontak/index.blade.php
+++ resources/views/kontak/index.blade.php
@@ -1,33 +1,110 @@
 @extends('layout.main')
 
 @section('styles')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
 <style>
-  #complaintsTable {
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
+  .contact .info-item {
+    padding: 10px;
+    padding-top: 20px;
   }
 
-  #complaintsTable th,
-  #complaintsTable td {
-    border: 1px solid #ddd;
-    padding: 8px;
+  .contact .info-item .social-icon i {
+    font-size: 10px;
+  }
+
+  .contact .info-item img {
+    width: auto;
+    height: 38px;
+  }
+
+  .tableBku {
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  .tableBku td,
+  .tableBku th {
+    padding: 0.7rem;
+    font-size: 0.87rem;
+    border-top: 1px solid #e3e6f0;
+  }
+
+  .tableBku th {
+    background-color: #E84545;
+    color: #fff;
+    font-weight: 700;
+  }
+
+  .tableBku td {
+    background-color: rgba(232, 69, 69, 0.05);
+  }
+
+  .tableBku thead th {
+    vertical-align: middle;
+    border-bottom: 2px solid #e3e6f0;
+    text-align: center;
+  }
+
+  .tableBku tbody+tbody {
+    border-top: 2px solid #e3e6f0;
+  }
+
+  .table-bordered {
+    border: 1px solid #e3e6f0;
+  }
+
+  .table-bordered td,
+  .table-bordered th {
+    border: 1px solid #e3e6f0;
+  }
+
+  .table-bordered thead td,
+  .table-bordered thead th {
+    border-bottom-width: 2px;
+  }
+
+  .label {
+    width: 100%;
     text-align: left;
   }
 
-  #complaintsTable th {
-    background-color: #f2f2f2;
+  .form-control {
+    display: inline-block;
+    width: 67%;
   }
 
-  #complaintsTable tbody tr:nth-child(even) {
-    background-color: #f2f2f2;
+  .kolomP {
+    min-width: 14rem;
   }
 
-  #complaintsTable tbody tr:hover {
-    background-color: #ddd;
+  .kolomS {
+    min-width: 9rem;
+  }
+
+  .kolomPS {
+    min-width: 22rem;
+  }
+
+  .kolomSS {
+    min-width: 3rem;
+  }
+
+  .table-responsive {
+    border-radius: 2%;
+    overflow-x: auto;
+    /* Ensure horizontal scrolling is enabled */
+  }
+
+  .Logo {
+    width: 3rem;
+  }
+
+  .text-normalsiku-100 {
+    color: #E84545;
+    font-weight: 600;
   }
 </style>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.bootstrap4.css">
 @endsection
 
 @section('container')
@@ -35,56 +112,68 @@
 
   <!-- Page Title -->
   <div class="page-title" data-aos="fade">
-    <div class="heading">
-      <div class="container">
+    <div class="heading" style="padding:0">
+      <div class="container-fluid"> <!-- Changed to container-fluid -->
         <div class="row d-flex justify-content-center text-center">
-          <div class="col-lg-8">
-            <h1 class="">COMING SOON</h1>
-            <p class="mb-0">Form Pengaduan bakal muncul disini</p>
+          <div class="col-lg-12">
+            <section id="contact" class="contact section">
+
+              <!-- Section Title -->
+              <div class="container section-title" data-aos="fade-up">
+                <h2>Pengaduan</h2>
+                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+              </div><!-- End Section Title -->
+
+              <div class="container-fluid" data-aos="fade-up" data-aos-delay="100" style="padding:0;"> <!-- Changed to container-fluid -->
+                <div class="card shadow mb-4">
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table class="tableBku table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                          <tr>
+                            <th class="kolomSS">NO.</th>
+                            <th class="kolomPS">JABATAN</th>
+                            <th class="kolomPS">NAMA</th>
+                            <th style="min-width: 4.4rem">NOMOR TELEPON</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>11</td>
+                            <td>KASUBAG</td>
+                            <td>Wiedyanto Andri Kusumo, S.Pi, MSE</td>
+                            <td><span><a href="#" class="btn btn-primary btn-user btn-circle btn-sm" style="margin-right: 0.1rem"><i class="bi bi-pencil-square"></i></a><a href="#" class="btn btn-danger btn-user btn-circle btn-sm"><i class="bi bi-trash"></i></a></span></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </section><!-- /Contact Section -->
+
           </div>
         </div>
       </div>
     </div>
   </div><!-- End Page Title -->
-
-  <!-- Data Table -->
-  <div class="container">
-    <table id="complaintsTable">
-      <thead>
-        <tr>
-          <th>Nomor</th>
-          <th>Jabatan</th>
-          <th>Nama</th>
-          <th>Nomor Lengkap</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Manajer</td>
-          <td>John Doe</td>
-          <td>08123456789</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Asisten Manajer</td>
-          <td>Jane Smith</td>
-          <td>08234567890</td>
-        </tr>
-        <!-- Tambahkan baris lain sesuai dengan kebutuhan -->
-      </tbody>
-    </table>
-  </div><!-- End Data Table -->
-
 </main>
 @endsection
 
 @section('scripts')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>
 <script>
-  $(document).ready(function() {
-    $('#complaintsTable').DataTable();
+  new DataTable('#dataTable', {
+    scrollX: true,
+    paging: true,
+    scrollCollapse: true,
+    scrollY: '570px',
+    "columnDefs": [{
+      "searchable": false,
+      "targets": 0
+    }]
   });
 </script>
 @endsection
